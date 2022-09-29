eb86fd97a4076ab7773ccc9cc8940870d7eb02f4
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2022-09-29 11:12:20 +0200

    Add a component to manage passengers using @ngrx/entity

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -20,12 +20,14 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import {EffectsModule} from "@ngrx/effects";
 import {RouterState, StoreRouterConnectingModule} from "@ngrx/router-store";
+import { PassengersModule } from './passengers/passengers.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     FlightBookingModule,
+    PassengersModule,
 
     BrowserAnimationsModule,
     FlightCancellingModule,
